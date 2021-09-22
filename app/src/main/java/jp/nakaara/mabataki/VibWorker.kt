3ab953ce0b7f9b6f0cefeaa1485c9593ee6faa28
kuuen <natukyouran@gmail.com>
2021-09-22 16:00:42 +0900

    アプリ一覧画面 アクティブなアプリを検出

--- app/src/main/java/jp/nakaara/mabataki/VibWorker.kt
+++ app/src/main/java/jp/nakaara/mabataki/VibWorker.kt
@@ -16,6 +16,7 @@ import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.content.getSystemService
 import androidx.work.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -32,6 +33,8 @@ class VibWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
          * workerを停止するにはtrueを指定
          */
         var halt = false
+
+        var instanceCount = 0
     }
 
     override fun doWork(): Result {
@@ -53,6 +56,17 @@ class VibWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
 //        Toast.makeText(this.applicationContext, "doWork呼び出し", Toast.LENGTH_SHORT).show()
 
+        instanceCount++;
+
+        // Workerを起動しっぱなしにするとWorkerのインスタンスが増えることがある
+        // Workerを終了していない場合、マネージャ?がそのWorkerを自動で起動しているような気がする
+        // 対処指定な場合Workerが複数同時に動作している状態となる
+        if (instanceCount >= 2) {
+            // その場合はループに入らず終了する
+            instanceCount--
+            return Result.success()
+        }
+
         // 暫く待つ　haltの値変更後は待ちが必要
         Thread.sleep(3500)
         halt = false
@@ -68,7 +82,7 @@ class VibWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
                 vibrator.vibrate(vibrationEffect)
             }, 1000)
 
-            Log.d(WORK_TAG, "リピート中")
+//            Log.d(WORK_TAG, "リピート中")
 
             val activityManager = this.applicationContext.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
             isForeground(activityManager)
@@ -93,8 +107,10 @@ class VibWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 //            for (activeProcess in processInfo.pkgList) {
 //                Log.d(WORK_TAG, processInfo.importance.toString())
 //
+//                Log.d(WORK_TAG, activeProcess.toString())
 //                if (processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
-//                    return true
+////                    return true
+//
 //                }
 //            }
 //        }
@@ -107,11 +123,24 @@ class VibWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
 
         val queryUsageStats = usageStatsManager.queryUsageStats(
-            UsageStatsManager.INTERVAL_DAILY, cal.timeInMillis, System.currentTimeMillis() // 3
+//            UsageStatsManager.INTERVAL_DAILY, cal.timeInMillis, System.currentTimeMillis() // 3
+            UsageStatsManager.INTERVAL_BEST, cal.timeInMillis, System.currentTimeMillis() // 3
         )
 
+        val sdf = SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss")
+//        Log.d(WORK_TAG, sdf.format(cal.getTime()))
+//        Log.d(WORK_TAG, sdf.format(System.currentTimeMillis()))
+
+        queryUsageStats.sortByDescending { i ->
+            i.lastTimeUsed
+        }
+
         queryUsageStats.forEach { i ->
-            Log.d(WORK_TAG, i.packageName)
+
+            if (i.lastTimeUsed >= 0) {
+                Log.d(WORK_TAG, i.packageName)
+                Log.d(WORK_TAG, Date(i.lastTimeUsed).toString())
+            }
         }
 
         return false
